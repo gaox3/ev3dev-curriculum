8065a27625b025b2dbf361052b22e8224e3d1363
gaox3 <gaox3@rose-hulman.edu>
2017-10-25 14:23:23 -0400

    gaox3

--- sandbox/gaox3/motors/m1_drive_timed.py
+++ sandbox/gaox3/motors/m1_drive_timed.py
@@ -8,7 +8,7 @@ to collect data, then make an equation to figure out the time needed given the d
 
 Note: If future modules you will learn different (BETTER) ways to drive a given distance.
 
-Authors: David Fisher and Xiang Gao & Ding Nie.
+Authors: David Fisher and Xiang Gao.
 """  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # DONE: 2. Get a yardstick or tape measure to do some testing with /examples/motors/drive_input_speed.py
