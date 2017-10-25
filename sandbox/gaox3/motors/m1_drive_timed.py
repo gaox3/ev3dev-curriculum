74cc9f0fd7147835d1d3e38f72694ca698b393b3
gaox3 <gaox3@rose-hulman.edu>
2017-10-25 15:12:20 -0400

    gaox3

--- sandbox/gaox3/motors/m1_drive_timed.py
+++ sandbox/gaox3/motors/m1_drive_timed.py
@@ -94,7 +94,11 @@ def main():
     time_s = 1  # Any value other than 0.
     while time_s != 0:
         speed = int(input("Enter a speed(0 to 900 dps): "))
+        if speed == 0:
+            break
         distance = int(input(" Enter a distance to travel (inches): "))
+        if distance == 0 :
+            break
         left_motor.run_forever(speed_sp=speed)
         right_motor.run_forever(speed_sp=speed)
         time.sleep(distance / (speed * 0.0108))
@@ -117,14 +121,14 @@ main()
 # to:
 #   Enter a speed (0 to 900 dps):
 #   Distance to travel (inches):
-# TODO: 5. Write the code necessary to make the robot drive at that speed going roughly that distance.
+# DONE: 5. Write the code necessary to make the robot drive at that speed going roughly that distance.
 #   Note, in this module, you are REQUIRED to use the pattern...
 #      run_forever()
 #      time.sleep(some_amount)
 #      stop()
 #   You may NOT use the advanced motor commands at this time like: run_to_abs_pos, run_to_rel_pos, or run_timed.
-# TODO: 6. Modify the program so that it will exit immediately if the answer to   any   question is 0.
-# TODO: 7. Formally test your work. When you think you have the problem complete run these tests to be sure:
+# DONE: 6. Modify the program so that it will exit immediately if the answer to   any   question is 0.
+# DONE: 7. Formally test your work. When you think you have the problem complete run these tests to be sure:
 #   200 dps 24 inches (make sure it drives within 6 inches of the target distance)
 #   400 dps 24 inches (make sure it drives within 6 inches of the target distance)
 #   800 dps 24 inches (make sure it drives within 6 inches of the target distance)
