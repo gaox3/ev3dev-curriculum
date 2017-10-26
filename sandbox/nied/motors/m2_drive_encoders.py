e2b337d38c66562d03517b5e9536a5e95b01a857
Ding <nied@rose-hulman.edu>
2017-10-26 14:20:06 -0400

    hi

--- sandbox/nied/motors/m2_drive_encoders.py
+++ sandbox/nied/motors/m2_drive_encoders.py
@@ -34,8 +34,8 @@ def main():
         position = int(input("Relative position to travel (inches):"))
         if position == 0:
             break
-        left_motor.run_to_rel_pos(speed_sp=speed, position_sp=position*90)
-        right_motor.run_to_rel_pos(speed_sp=speed, position_sp=position*90)
+        left_motor.run_to_rel_pos(speed_sp=speed, position_sp=position*90,stop_action= ev3.Motor.STOP_ACTION_COAST)
+        right_motor.run_to_rel_pos(speed_sp=speed, position_sp=position*90,stop_action= ev3.Motor.STOP_ACTION_COAST)
         left_motor.wait_while(ev3.Motor.STATE_RUNNING)
         right_motor.wait_while(ev3.Motor.STATE_RUNNING)
         ev3.Sound.beep().wait()
