692625ebd58b393badef9d7e0f5b56b00f9c079a
Ding <nied@rose-hulman.edu>
2017-11-01 21:46:22 -0400

    hi

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -53,8 +53,8 @@ class Snatch3r(object):
         self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
 
     def stop(self):
-        self.left_motor.stop(stop_action=ev3.Motor.STOP_ACTION_BRAKE)
-        self.right_motor.stop(stop_action=ev3.Motor.STOP_ACTION_BRAKE)
+        self.left_motor.stop(stop_action=ev3.Motor.STOP_ACTION_COAST)
+        self.right_motor.stop(stop_action=ev3.Motor.STOP_ACTION_COAST)
 
     def forward(self, left_speed_entry, right_speed_entry):
         self.left_motor.run_forever(speed_sp=left_speed_entry)
