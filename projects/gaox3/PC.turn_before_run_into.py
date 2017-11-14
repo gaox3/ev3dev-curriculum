125672bcf049b385bda9bb89e8b99cff0d0ad583
gaox3 <gaox3@rose-hulman.edu>
2017-11-14 13:56:37 -0500

    Final project

---
+++ projects/gaox3/PC.turn_before_run_into.py
@@ -0,0 +1,50 @@
+
+import tkinter
+from tkinter import ttk
+import mqtt_remote_method_calls as com
+
+
+class MyDelegatePC(object):
+    def __init__(self):
+        self.running = True
+
+
+def main():
+    root = tkinter.Tk()
+    root.title("MQTT Remote")
+    main_frame = ttk.Frame(root, padding=20, relief='raised')
+    main_frame.grid()
+
+    my_delegate = MyDelegatePC()
+    mqtt_client = com.MqttClient(my_delegate)
+    mqtt_client.connect_to_ev3()
+
+    start_button = ttk.Button(main_frame, text="Go!")
+    start_button.grid(row=1, column=1)
+    start_button['command'] = lambda: start(mqtt_client)
+    root.bind('<space>', lambda event: start(mqtt_client))
+
+    stop_button = ttk.Button(main_frame, text="Stop")
+    stop_button.grid(row=1, column=3)
+    stop_button['command'] = lambda: stop(mqtt_client)
+    root.bind('<space>', lambda event: stop(mqtt_client))
+
+
+def start(mqtt_client):
+    print("Go!")
+    mqtt_client.send_message("start")
+
+def stop(mqtt_client):
+    print("stop")
+    mqtt_client.send_message("stop")
+
+
+
+
+
+
+
+
+
+main()
+
--- projects/gaox3/Turn_before_run_into.py
+++ projects/gaox3/Turn_before_run_into.py
@@ -1,6 +1,9 @@
 import ev3dev.ev3 as ev3
 import time
 
+import robot_controller as robo
+import mqtt_remote_method_calls as com
+
 
 def main():
     ev3.Sound.speak("Avoid collision system activated").wait()
@@ -8,6 +11,22 @@ def main():
     right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
     assert left_motor.connected
     assert right_motor.connected
+
+    # Robot
+    robot = robo.Snatch3r()
+    # Remote control
+    mqtt_client = com.MqttClient(robot)
+    mqtt_client.connect_to_pc()
+
+    while True:
+        if robot.ir_sensor.proximity < 10:
+            robot.left_motor.stop(stop_action=ev3.Motor.STOP_ACTION_COAST)
+            robot.right_motor.stop(stop_action=ev3.Motor.STOP_ACTION_COAST)
+            time.sleep(1)
+            robot.turn_degrees(90, 200)
+        else:
+            left_motor.run_forever(speed_sp=400)
+            right_motor.run_forever(speed_sp=400)
 
 
 def drive_forward(left_motor, right_motor, time_s):
@@ -17,5 +36,5 @@ def drive_forward(left_motor, right_motor, time_s):
     left_motor.stop(stop_action="brake")
     right_motor.stop(stop_action="brake")
 
-while True:
-    
+
+main()
